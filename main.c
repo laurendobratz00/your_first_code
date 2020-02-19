930c86b36e030b4f7263453b9f1b9a842f17c01b
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2020-02-19 12:01:55 -0600

    Hello-Holberton
    from Holberton to Hello Holberton

--- main.c
+++ main.c
@@ -1,35 +1,32 @@
-{
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("Hello Holberton\n");
-  }
-  return 0;
-}
-@@ -3,7 +3,29 @@ int main()
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("Hello Holberton\n");
-  }
-  return 0;
-}
-@@ -3,7 +3,29 @@ int main()
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("Hello Holberton\n");
-  }
-  return 0;
-}
-@@ -3,7 +3,29 @@ int main()
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("Hello Holberton\n");
-  }
-  return 0;
-}
-
-
-
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
+Hello%20Holberton
