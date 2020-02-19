93dcc11ca5c6fbab3cf763969fe17f16b977a473
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2020-02-19 11:56:03 -0600

    Hello-Holberton
    from Holberton to Hello Holberton

--- main.c
+++ main.c
@@ -1,9 +1,35 @@
-int main()
 {
   int i;
   for (i = 0; i < 10; i++)
   {
-    printf("Holberton\n");
+    printf("Hello Holberton\n");
   }
   return 0;
 }
+@@ -3,7 +3,29 @@ int main()
+  int i;
+  for (i = 0; i < 10; i++)
+  {
+    printf("Hello Holberton\n");
+  }
+  return 0;
+}
+@@ -3,7 +3,29 @@ int main()
+  int i;
+  for (i = 0; i < 10; i++)
+  {
+    printf("Hello Holberton\n");
+  }
+  return 0;
+}
+@@ -3,7 +3,29 @@ int main()
+  int i;
+  for (i = 0; i < 10; i++)
+  {
+    printf("Hello Holberton\n");
+  }
+  return 0;
+}
+
+
+
